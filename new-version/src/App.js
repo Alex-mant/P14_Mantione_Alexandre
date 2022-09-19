18f0865d2c6e6930e42725e68d51d910a6d19c60
Alex Mant <al.mantione@gmail.com>
2022-09-19 18:13:06 +0200

    Redux Provider added

--- new-version/src/App.js
+++ new-version/src/App.js
@@ -1,16 +1,22 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import HRnetLogo from "./components/HRnetLogo/HRnetLogo";
 import EmployeeList from "./pages/EmployeeList";
 import Home from "./pages/Home";
+import { store } from "./redux/app/store";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path="/home" element={<Home/>}/>
-          <Route path="/employee-list" element={<EmployeeList/>}/>
-        </Routes>
-      </Router>
+      <Provider store={store}>
+        <HRnetLogo/>
+        <Router>
+          <Routes>
+            <Route exact path="/home" element={<Home/>}/>
+            <Route path="/employee-list" element={<EmployeeList/>}/>
+          </Routes>
+        </Router>
+      </Provider>
     </div>
   );
 }
