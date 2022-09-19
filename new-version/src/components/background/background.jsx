d8c761b8604ffe2dbfe76441d78d485d1acafc52
Alex Mant <al.mantione@gmail.com>
2022-09-19 18:15:55 +0200

    background integration

---
+++ new-version/src/components/background/background.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.scss';
+
+ const Background = () => {
+  return (
+    <div id='background'>
+      <div className='a'></div>
+      <div className='b'></div>
+      <div className='c'></div>
+      <div className='d'></div>
+    </div>
+  );
+}
+
+export default Background;
