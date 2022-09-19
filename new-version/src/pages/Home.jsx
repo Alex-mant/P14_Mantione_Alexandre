f8240b345c282f6432bbd3f90fc52ebb0d3de6f6
Alex Mant <al.mantione@gmail.com>
2022-09-19 18:17:53 +0200

    Home & Employee pages added

--- new-version/src/pages/Home.jsx
+++ new-version/src/pages/Home.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import FormToCreateEmployee from '../components/FormToCreateEmployee/FormToCreateEmployee';
+import ModalConfirmation from '../components/ModalConfirmation/ModalConfirmation';
 
 const Home = () => {
   const formTitle = 'Create Employee';
-
-  const handleClick = () => {
-    const x = document.querySelector(".Form");
-    const formData = new FormData(x);
-    console.log(...formData.entries());
-  }
+  const css = {id:"Modal"}
+  const text = "OKOK";
 
   return (
     <div id='home'>
-      <h1 className='home_mainTitle'>HRnet</h1>
       <Link className='home_navLink' to={{pathname:'/employee-list'}}>View Current Employees</Link>
       <FormToCreateEmployee title={formTitle}/>
-      <button onClick={() => {handleClick()}} className='home_submit-btn'>Save</button>
+      <ModalConfirmation css={css} text={text}/>
     </div>
   );
 }
