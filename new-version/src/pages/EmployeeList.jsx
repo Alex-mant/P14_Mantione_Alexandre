e8b0b064552c9ac2b1b4ace7bbb6cbfe672af287
Alex Mant <al.mantione@gmail.com>
2022-09-28 18:11:41 +0200

    Preparation plugin table 'rc-table'

--- new-version/src/pages/EmployeeList.jsx
+++ new-version/src/pages/EmployeeList.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
+import Table from 'rc-table'
+import styled from 'styled-components'
+import { useSelector } from 'react-redux';
 
 const EmployeeList = () => {
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'first-name',
+      key: 'first-name',
+      width: 100,
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+      width: 100,
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+      width: 200,
+    },
+    {
+      title: 'Operations',
+      dataIndex: '',
+      key: 'operations',
+      render: () => <a href="#">Delete</a>,
+    },
+  ];
+
+  const data = useSelector(state => state.employee.employeeList);
+
+  const Test = styled.div`
+    margin-top: 400px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `
   return (
-    <div>
-      <h1>EMPLOYEELIST</h1>
-    </div>
+    <Test>
+      <Table columns={columns} data={data}/>
+    </Test>
   );
 }
 
