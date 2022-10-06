b52afbbcea5d79c085de51692fee9f02e16f6219
Alex Mant <al.mantione@gmail.com>
2022-10-06 18:21:21 +0200

    refactor: creation of a selectPlugin component
    creation of a selectPlugin component

---
+++ new-version/src/components/SelectPlugin/SelectPlugin.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Select from 'react-select';
+import { setFormDepartment, setFormState } from '../../redux/feature/formularySlice';
+import { getInfoFromForm } from '../../utils/getInfoFromForm';
+
+const SelectPlugin = ({object}) => {
+  const dispatch = useDispatch();
+  const select = object.select;
+  const placeHolder = () => 
+  select.name === "state" ? currentUserState : currentUserDepartment;
+  const step = useSelector((state) => state.formulary.step);
+  const currentUserState = useSelector((state) => state.formulary.state);
+  const currentUserDepartment =  useSelector((state) => state.formulary.department);
+
+  const handleChange = (e) => {
+    getInfoFromForm(e, dispatch, step);
+    select.name === "state" ? dispatch(setFormState(e.label)) : select.name === "department" ? dispatch(setFormDepartment(e.label)) : <></>;
+  }
+
+  return (
+    <Select classNamePrefix="react-select" onChange={(e) => handleChange(e)} name={select.name} id={'select'} placeholder={placeHolder()} options={select.options} />
+  );
+}
+
+export default SelectPlugin;
