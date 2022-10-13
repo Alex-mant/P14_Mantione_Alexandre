83e59e961b94a114f8bce36e4d637058e586de6d
Alex Mant <al.mantione@gmail.com>
2022-10-13 13:03:41 +0200

    refactor(lazy & suspense removed from tablePlugin file):

--- new-version/src/components/TablePlugin/TablePlugin.jsx
+++ new-version/src/components/TablePlugin/TablePlugin.jsx
@@ -1,18 +1,13 @@
-import React, { useCallback, useRef, Suspense, useMemo } from "react";
+import React, { useCallback, useRef, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import { columns } from "../../data/columnsTable";
 import { handleCellContextMenu } from "../../utils/handleCellContextMenu";
 import { useState } from "react";
+import { AgGridReact } from "ag-grid-react";
+import TableTools from "../TableTools/TableTools";
 
-const TableTools = React.lazy(() => import("../TableTools/TableTools"));
-
-const AgGridReact = React.lazy(() =>
-  import("ag-grid-react").then((module) => {
-    return { default: module.AgGridReact };
-  })
-);
 const colWidth = 150;
 const containerWidth = colWidth * 9;
 
@@ -26,28 +21,28 @@ const TablePlugin = () => {
     []
   );
 
-  const [numberOfRows, setNumberOfRows] = useState(540);
+  const [numberOfRows, setNumberOfRows] = useState();
   const dispatch = useDispatch();
-  const containerStyle = useMemo(() => ({ width: containerWidth, height: numberOfRows, maxHeight: 520, minHeight: 260 }), [numberOfRows]);
+  const containerStyle = useMemo(() => ({ width: containerWidth, height: numberOfRows, maxHeight: 520, minHeight: 147 }), [numberOfRows]);
   const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
   const gridRef = useRef();
   const data = useSelector((state) => state.formulary.List);
   const filteredList = useSelector((state) => state.formulary.filteredList);
-  
+
   const paginationNumberFormatter = useCallback((params) => {
-    setNumberOfRows(params.value * 49 + 98);
+    setNumberOfRows(params.value* 44 + 98);
     return "[" + params.value.toLocaleString() + "]";
   }, []);
-  
+
   return (
     <>
       <div style={containerStyle}>
         <div className="ag-theme-alpine" style={gridStyle}>
-          <Suspense fallback={<div>Loading...</div>}>
             <TableTools gridRef={gridRef} />
             <AgGridReact
+              overlayNoRowsTemplate="No Data"
               rowSelection="single"
-              onCellClicked={(e) => handleCellContextMenu(e, dispatch, data )}
+              onCellClicked={(e) => handleCellContextMenu(e, dispatch, data)}
               preventDefaultOnContextMenu
               suppressHorizontalScroll
               fullWidthCellRenderer={10}
@@ -60,9 +55,8 @@ const TablePlugin = () => {
               columnDefs={columns}
               defaultColDef={defaultColDef}
             />
-          </Suspense>
-      </div>
         </div>
+      </div>
     </>
   );
 };
