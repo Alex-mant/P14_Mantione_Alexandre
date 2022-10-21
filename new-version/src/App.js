6e89171ddf993fe211baeaa7b1570213010817b0
Alex Mant <al.mantione@gmail.com>
2022-10-21 15:48:35 +0200

    feat: redux-persist was added

--- new-version/src/App.js
+++ new-version/src/App.js
@@ -3,23 +3,29 @@ import { Provider } from "react-redux";
 import HRnetLogo from "./components/HRnetLogo/HRnetLogo";
 import { store } from "./redux/app/store";
 import React, { Suspense } from "react";
+import persistStore from "redux-persist/es/persistStore";
+import { PersistGate } from 'redux-persist/integration/react'
 
 const Home = React.lazy(() => import("./pages/Home"))
 const EmployeeList = React.lazy(() => import("./pages/EmployeeList"))
+
+let persistor = persistStore(store)
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <HRnetLogo />
-        <Router>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-              <Route exact path="/home" element={<Home />} />
-              <Route path="/employee-list" element={<EmployeeList />} />
-            </Routes>
-          </Suspense>
-        </Router>
+        <PersistGate loading={null} persistor={persistor}>
+          <HRnetLogo />
+          <Router>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Routes>
+                <Route exact path="/home" element={<Home />} />
+                <Route path="/employee-list" element={<EmployeeList />} />
+              </Routes>
+            </Suspense>
+          </Router>
+        </PersistGate>
       </Provider>
     </div>
   );
--- new-version/src/redux/app/store.js
+++ new-version/src/redux/app/store.js
@@ -1,8 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { FormularySlice } from "../feature/formularySlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, FormularySlice.reducer);
 
 export const store = configureStore({
   reducer: {
-    formulary: FormularySlice.reducer,
+    formulary: persistedReducer,
   },
 });
