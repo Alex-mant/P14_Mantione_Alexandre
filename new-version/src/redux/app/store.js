a3c1e2bc7cecf362dc70fb7438b7aa3e9fcd93f4
Alex Mant <al.mantione@gmail.com>
2022-10-21 16:28:32 +0200

    feat: redux-store : middleWare added

--- new-version/src/redux/app/store.js
+++ new-version/src/redux/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { FormularySlice } from "../feature/formularySlice";
 import storage from "redux-persist/lib/storage";
-import { persistReducer } from "redux-persist";
+import { FLUSH, PAUSE, PERSIST, persistReducer, PURGE, REGISTER, REHYDRATE } from "redux-persist";
 
 const persistConfig = {
   key: "root",
@@ -15,4 +15,10 @@ export const store = configureStore({
   reducer: {
     formulary: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
