6dc6e8ca2ef1b0dbfdd205dc03d143b18c007d91
Alex Mant <al.mantione@gmail.com>
2022-10-28 10:30:05 +0200

    feat: 404 error page added

--- new-version/src/App.js
+++ new-version/src/App.js
@@ -4,10 +4,11 @@ import HRnetLogo from "./components/HRnetLogo/HRnetLogo";
 import { store } from "./redux/app/store";
 import React, { Suspense } from "react";
 import persistStore from "redux-persist/es/persistStore";
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react';
 
-const Home = React.lazy(() => import("./pages/Home"))
-const EmployeeList = React.lazy(() => import("./pages/EmployeeList"))
+const Home = React.lazy(() => import("./pages/Home"));
+const EmployeeList = React.lazy(() => import("./pages/EmployeeList"));
+const Error404 = React.lazy(() => import("./pages/Error404"));
 
 let persistor = persistStore(store)
 
@@ -22,6 +23,7 @@ function App() {
               <Routes>
                 <Route exact path="/home" element={<Home />} />
                 <Route path="/employee-list" element={<EmployeeList />} />
+                <Route path="*" element={<Error404/>} />
               </Routes>
             </Suspense>
           </Router>
---
+++ new-version/src/pages/Error404.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const PageContainer = styled.div`
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  text-align: center;
+  transform: translate(-50%, -50%);
+  h2{
+    font-size: 40px;
+    margin-bottom: 0;
+  }
+  p{
+    font-size: 20px;
+  }
+  a{
+    text-decoration: none;
+    color: #000;
+    &:hover{
+      color: #768E12;
+    }
+  }
+`
+
+const Error404 = () => {
+  return (
+    <PageContainer id='Error404'>
+      <h2>Error 404</h2>
+      <p>Oops 404 - this page doesn't exist or it has been removed</p>
+      <Link to='/home'>Back to homepage here !</Link>
+    </PageContainer>
+  );
+}
+
+export default Error404;
